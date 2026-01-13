44d2663ba02269e1bc717415d3349891cc771165
Pankaj Bhatt <pankajthekiwi@hotmail.com>
2026-01-13 08:15:01 +0530

    Basic User Login Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: "Home - Robot Shop" },
   { path: 'catalog', component: CatalogComponent, title: "Catalog - Robot Shop" },
   { path: 'cart', component: CartComponent, title: "Cart - Robot Shop" },
-  {path: '', redirectTo: '/home', pathMatch: 'full'}
+  {path: 'sign-in', component: SignInComponent, title: "Login - Robot Shop" },
+  {path: '', redirectTo: '/home', pathMatch: 'full'}  
 ];
 
 @NgModule({
